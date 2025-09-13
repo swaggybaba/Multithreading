00ee2066fe63f27a1553c86238e6a7811c4ff906
Prafful Chauhan <74653860+swaggybaba@users.noreply.github.com>
2025-09-13 16:54:46 +0530

    Create SortOddAndEven.java

---
+++ SortAndMergeUsingThreading/SortOddAndEven.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SortOddAndEven implements Runnable{
+    private final int arr[];
+    private final boolean isOdd;
+    private int sortedArray[];
+
+    public SortOddAndEven(final int[] arr, final boolean isOdd) {
+        this.arr = arr;
+        this.isOdd = isOdd;
+    }
+
+    @Override
+    public void run() {
+        sortedArray = Arrays.stream(arr).filter(isOdd ? (a -> a%2 != 0) : (a -> a%2 == 0)).sorted().toArray();
+    }
+
+    public int[] getSortedArray() {
+        return sortedArray;
+    }
+}
